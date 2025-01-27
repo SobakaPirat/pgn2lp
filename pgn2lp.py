636c9a5505ef0d40e3ff1da072aee6b191c9c531
SobakaPirat <egor.pudov@bk.ru>
2025-01-27 16:20:18 +0300

    feat: filter players, if they are in players_to_filter.txt

--- pgn2lp.py
+++ pgn2lp.py
@@ -1,7 +1,16 @@
 from chess.pgn import read_game
 
 # Specify the name of your file here
-pgn_file_path = 'tournament2.pgn'
+pgn_file_path = 'tournament.pgn'
+
+# Open and read the file with the players to be filtered
+def fetch_players_to_filter():
+    try:
+        with open('players_to_filter.txt', 'r', encoding='utf-8') as f:
+            players_to_filter = [line.strip() for line in f.readlines()]
+            return players_to_filter
+    except:
+        return False
 
 def swap_name_surname(full_name):
     # Split the string by comma
@@ -19,6 +28,7 @@ def swap_name_surname(full_name):
     return f"{name} {surname}"
 
 def parse_pgn_to_wikicode(file_path):
+    players_to_filter = fetch_players_to_filter()
     matches = []
     with open(file_path, 'r', encoding='utf-8') as file:
         while True:
@@ -34,6 +44,10 @@ def parse_pgn_to_wikicode(file_path):
             result = game.headers.get("Result", "Unknown")
             eco = game.headers.get("ECO", "Unknown")
             moves = game.end().board().fullmove_number
+            # Check if either player is one of the players to filter
+            if players_to_filter:
+                if white not in players_to_filter and black not in players_to_filter:
+                    continue
             # Determining the winner
             if result == "1-0":
                 winner = "1"
